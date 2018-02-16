447c8fdef1e8e6145701eda74a3169d14822e5d2
Geoff Garbers <geoff@garbers.co.za>
2018-02-16 07:15:41 +0200

    Fix incorrect reference to non-existent tag for deployment.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,6 +2,6 @@
 set -e
 
 docker push ${REPO_NAME}/mail:latest
-docker push ${REPO_NAME}/mail:latest-${COMMIT}
+docker push ${REPO_NAME}/mail:${COMMIT}
 docker push ${REPO_NAME}/mail:debug
 docker push ${REPO_NAME}/mail:debug-${COMMIT}
